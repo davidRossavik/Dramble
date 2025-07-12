d3f85261e6fc36de70c6059c8b1fc741333fe428
Trym Johnsen <trymajo@stud.ntnu.no>
2025-07-12 14:17:47 +0200

    lagt til realtime lytting for current_challenge_index kolonne i supabase

--- app/challengeScreen.tsx
+++ app/challengeScreen.tsx
@@ -1,12 +1,15 @@
+import { supabase } from '@/supabase';
 import { Challenge } from '@/utils/types'; // hvis du har laget typen Challenge
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect } from 'react';
 import { Text } from 'react-native';
 import OneVsOne from './challengetypes/OneVsOne';
 import TeamVsItself from './challengetypes/TeamVsItself';
 import TeamVsTeam from './challengetypes/TeamVsTeam';
 
 export default function ChallengeScreen() {
-  const { challenge: challengeParam } = useLocalSearchParams();
+  const { challenge: challengeParam, gameId } = useLocalSearchParams();
+  const router = useRouter();
 
   if (typeof challengeParam !== 'string') {
     return <Text>Ugyldig challenge-data</Text>;
@@ -18,6 +21,52 @@ export default function ChallengeScreen() {
   } catch (err) {
     return <Text>Feil i challenge-formatet</Text>;
   }
+
+
+  // følger med på endring av current_challenge_index og kjører ny challenge skjerm når index oppdateres
+  useEffect(() => {
+    if (!gameId || typeof gameId !== 'string') return;
+
+    const channel = supabase
+      .channel(`game-progress-${gameId}`)
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'games',
+          filter: `id=eq.${gameId}`,
+        },
+        async (payload) => {
+            const oldindex = payload.old.current_challenge_index;
+            const newindex = payload.new.current_challenge_index;
+
+            if (oldindex === newindex) {
+                return;
+            }
+            
+            const challenges = payload.new.challenges;
+            const nextChallenge = challenges[newindex];
+
+            if (!nextChallenge) return;
+
+            router.replace({
+                pathname: '/challengeScreen',
+                params: {
+                  challenge: JSON.stringify(nextChallenge),
+                  gameId,
+                },
+            });
+            }
+        )
+        .subscribe();
+
+        return () => {
+            supabase.removeChannel(channel);
+        };
+    }, [gameId]);
+
+
 
   switch (parsedChallenge.type) {
     case '1v1':
