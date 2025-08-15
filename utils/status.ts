1ece4ed1027d7e91cd09f52a83de6f6aea84742f
David Rossavik <rossavikdavid@gmail.com>
2025-08-15 14:07:44 +0200

    10 runder

--- utils/status.ts
+++ utils/status.ts
@@ -25,7 +25,7 @@ export async function updateGameStatus(gameId: string, status: 'waiting' | 'play
 
 //kjøres når hosten trykker på start spill første gang
 export async function initializeGame(gameId: string) {
-  const challenges = await getRandomChallengesWithPlayers(gameId, 1);
+  const challenges = await getRandomChallengesWithPlayers(gameId, 10);
 
   // Hent alle lag for å velge automatisk
   const { data: game, error: gameError } = await supabase
